ee53f1a40df53bde39a6730c37833e8093e3840e
fruit13ok <fruit13ok@gmail.com>
2019-03-01 18:21:37 -0800

    Wrap component, refer to child

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-
 // import other component
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     let name1 = 'Sam';
@@ -11,22 +11,37 @@ const App = () => {
     let name3 = 'Jose';
     return (
         <div className="ui container comments">
+            {/* ApprovalCard component use for styling, 
+            wraping the content, which is CommentDetail,
+            use open and close tags instead of self close tag */}
+            <ApprovalCard>
+                <div>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author={name1} 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice" 
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
             {/* passing porp name author with literal value or variable inside {} */}
-            <CommentDetail 
-                author={name1} 
-                timeAgo="Today at 4:45PM" 
-                content="Nice" 
-                avatar={faker.image.avatar()} />
-            <CommentDetail 
-                author={name2} 
-                timeAgo="Today at 2:00PM" 
-                content="Ok" 
-                avatar={faker.image.avatar()} />
-            <CommentDetail 
-                author={name3} 
-                timeAgo="Today at 12:59PM" 
-                content="Wow" 
-                avatar={faker.image.avatar()} />
+            <ApprovalCard>
+                <CommentDetail 
+                    author={name2} 
+                    timeAgo="Today at 2:00PM" 
+                    content="Ok" 
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author={name3} 
+                    timeAgo="Today at 12:59PM" 
+                    content="Wow" 
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 }
