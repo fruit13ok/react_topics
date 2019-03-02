7b44fe4247cc23aac5e30a95a4c2b8d4bc66a5fd
fruit13ok <fruit13ok@gmail.com>
2019-03-01 16:46:23 -0800

    pass multiple props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import faker from 'faker';
 
 // component will have build in argument name props, pass in props as list of objects
 const CommentDetail = (props) => {
   return (
     <div className="comment">
         <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}></img>
+            <img alt="avatar" src={props.avatar}></img>
         </a>
         <div className="content">
             <a href="/" className="author">
@@ -14,9 +13,9 @@ const CommentDetail = (props) => {
                 {props.author}
             </a>
             <div className="metadata">
-                <span className="date">Today at 6:00PM</span>
+                <span className="date">{props.timeAgo}</span>
             </div>
-            <div className="text">Nice blog post</div>
+            <div className="text">{props.content}</div>
         </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // import other component
 import CommentDetail from './CommentDetail';
 
 const App = () => {
-    let name1 = 'Sam1';
+    let name1 = 'Sam';
+    let name2 = 'John';
+    let name3 = 'Jose';
     return (
         <div className="ui container comments">
             {/* passing porp name author with literal value or variable inside {} */}
-            <CommentDetail author={name1} />
-            <CommentDetail author='Sam2' />
-            <CommentDetail author='Sam3' />
+            <CommentDetail 
+                author={name1} 
+                timeAgo="Today at 4:45PM" 
+                content="Nice" 
+                avatar={faker.image.avatar()} />
+            <CommentDetail 
+                author={name2} 
+                timeAgo="Today at 2:00PM" 
+                content="Ok" 
+                avatar={faker.image.avatar()} />
+            <CommentDetail 
+                author={name3} 
+                timeAgo="Today at 12:59PM" 
+                content="Wow" 
+                avatar={faker.image.avatar()} />
         </div>
     );
 }
